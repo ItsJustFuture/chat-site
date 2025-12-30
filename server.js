ad7f284f0170601d82d29ebd26db4f7fbc89973a
ItsJustFuture <kidtails@yahoo.com>
2025-12-30 15:30:51 +1300

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const io = new Server(server, {
   cors: { origin: true, credentials: true },
 });
 app.use(express.json({ limit: "1mb" }));
-app.use(express.urlencoded({ extended: true })
+app.use(express.urlencoded({ extended: true }))
 const pgPool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === "production"
